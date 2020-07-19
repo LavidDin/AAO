8b3d17f41efd14bb1317f845660346ca960078f1
David Lin <David.lin71@gmail.com>
2020-07-19 06:48:31 -0400

    memory puzzle

--- ruby/memory_puzzle/board.rb
+++ ruby/memory_puzzle/board.rb
@@ -1,7 +1,7 @@
-require relative "card"
+require_relative "card"
 
 class Board
-attr_reader :size
+  attr_reader :size
 
   def initialize(size = 4)
     @rows = Array.new(size) { Array.new(size) }
@@ -10,32 +10,57 @@ attr_reader :size
   end
 
   def [](pos)
-      row, col = pos
-      rows[row][col]
+    row, col = pos
+    rows[row][col]
   end
 
-  def []=(pos, val)
-      row, col = pos
-      rows[row][col] = value
+  def []=(pos, value)
+    row, col = pos
+    rows[row][col] = value
+  end
+
+  def hide(pos)
+    self[pos].hide
+  end
+
+  def reveal(pos)
+    if revealed?(pos)
+      puts "You can't flip a card that has already been revealed."
+    else
+      self[pos].reveal
+    end
+    self[pos].value
   end
 
   def populate
-
+    num_pairs = (size**2) / 2
+    cards = Card.shuffled_pairs(num_pairs)
+    rows.each_index do |i|
+      rows[i].each_index do |j|
+        self[[i, j]] = cards.pop
+      end
+    end
   end
 
   def render
+    system("clear")
+    puts "  #{(0...size).to_a.join(' ')}"
+    rows.each_with_index do |row, i|
+      puts "#{i} #{row.join(' ')}"
+    end
+  end
 
+  def revealed?(pos)
+    self[pos].revealed?
   end
 
   def won?
-
+    rows.all? do |row|
+      row.all?(&:revealed?)
+    end
   end
 
-  def reveal
+  private
 
-  end
-
-
-
-
+  attr_reader :rows
 end
