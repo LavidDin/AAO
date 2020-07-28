bc9c0a3bb0a18b2b2fdf11d9070b26d057dab46f
David Lin <David.lin71@gmail.com>
2020-07-28 14:36:18 -0400

    practice writing dfs and bfs algorithms

--- ruby/diy_adts.rb
+++ ruby/diy_adts.rb
@@ -39,36 +39,36 @@ end
 
 #Exercise 3 - Map
 class Map
-def initialize
-    @underlying_array = []
-end
+    def initialize
+        @underlying_array = []
+    end
 
-def set(key, value)
-pair_index = underlying_array.index { |pair| pair[0] == key }
-if pair_index
-    underlying_array[pair_index][1] = value
-else
-    underlying_array.push([key, value])
-end
-value
-end
-
-def get(key)
-    underlying_array.each { |pair| return pair[1] if pair[0] == key }
-    nil
-end
-
-def delete(key)
-    value = get(key)
-    underlying_array.reject! { |pair| pair[0] == key }
+    def set(key, value)
+        pair_index = underlying_array.index { |pair| pair[0] == key }
+        if pair_index
+            underlying_array[pair_index][1] = value
+        else
+            underlying_array.push([key, value])
+        end
     value
-end
+    end
 
-def show
-    deep_dup(underlying_array)
-end
+    def get(key)
+        underlying_array.each { |pair| return pair[1] if pair[0] == key }
+        nil
+    end
 
-def deep_dup(arr)
-  arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
-end  
+    def delete(key)
+        value = get(key)
+        underlying_array.reject! { |pair| pair[0] == key }
+        value
+    end
+
+    def show
+        deep_dup(underlying_array)
+    end
+
+    def deep_dup(arr)
+        arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+    end  
 end
